4082a38d026fabbaa7bcc784cb4f2407fc7db468
saimca-75 <necnmca75@gmail.com>
2026-01-13 12:41:47 +0530

    Leson 1 - Activity 1

---
+++ M1/Lesson1/Lesson1 - Activity 1/main.py
@@ -0,0 +1,8 @@
+#the first print statement
+print("Hello, World!")
+
+#the second print statement
+print("Welcome to Python programming.")
+
+#the third print statement
+print("Let's learn together!")
