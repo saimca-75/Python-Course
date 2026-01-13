b38e80b5010512f5f9830625e8b3eb0244a0dea1
saimca-75 <necnmca75@gmail.com>
2026-01-14 00:39:34 +0530

    "lesson2 actvity1"

---
+++ M1/Lesson2/Lesson2_activity1/print_statements.py
@@ -0,0 +1,19 @@
+# printing a sentence e.g.
+
+print("Hi there what is your name?")
+
+#print a number
+
+print(13)
+
+#next line after print
+
+print("pypy \n Dont call me that ")
+
+#passing mutli arguments in print statement
+
+print(" I'm 13", 5)
+
+#argument(value/ variable of a print statement
+
+print("Pi OS", end = y)
