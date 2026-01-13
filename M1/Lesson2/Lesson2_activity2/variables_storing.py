162ea47df6bc06ca921e1a4fafd4e785894da919
saimca-75 <necnmca75@gmail.com>
2026-01-14 00:53:42 +0530

    "lesson2 activity 2"

---
+++ M1/Lesson2/Lesson2_activity2/variables_storing.py
@@ -0,0 +1,16 @@
+
+#storing adifferent values and variables
+
+x = "13"
+
+y = 9
+
+print(x, y)
+
+pea = "veggi"
+
+print(pea)
+
+user_Input = input("Hi waht is ur name")
+
+print("nice to meet u",user_Input)
